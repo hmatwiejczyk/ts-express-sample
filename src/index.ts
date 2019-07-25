b141cdbc359db1b2840d706e18d15d840f64ef87
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-26 00:41:53 +0200

    add simple server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi there');
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>Hello!</div>
+  `);
+});
+
+app.listen(3000, () => console.log('listening on 3000'));
