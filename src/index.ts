65ee787ad6789332484114494a4fc3796d3a55ea
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-31 00:36:06 +0200

    add login route

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
 
 const app = express();
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>Hello!</div>
-  `);
-});
+app.use(router);
 
 app.listen(3000, () => console.log('listening on 3000'));
