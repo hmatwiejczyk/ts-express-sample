330abdcc6e29955301e06d19fb44787ea0a068dd
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-31 01:07:14 +0200

    dealing with poor type def

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,28 @@
-import { Router } from "express";
+import { Router, Request, Response } from 'express';
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('hi there');
-})
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <input name="email" placeholder="email"/>
+      <input name="password" type="password" placeholder="password" />
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+  if (email) {
+    res.send(email.toUpperCase() + '  ' + password);
+  } else {
+    res.send('you must provide email')
+  }
+});
 
 export { router };
